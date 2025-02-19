a66932db706e3819be97ed7101e8f128094edc44
nbd11 <nicolasdestephano@ucsb.edu>
2025-02-18 17:29:00 -0800

    Reformatting to include intercropping data set and responsive yield vs. time chart

--- shiny/a2t3_shiny_intercropping.R
+++ shiny/a2t3_shiny_intercropping.R
@@ -1,56 +1,69 @@
 library(tidyverse)
-library(palmerpenguins)
 library(shiny)
 library(bslib)
+
+### load data
+intercrop <- read_delim(delim = ';', here("data", "Database.csv"))
+
+### Convert relevant columns to numeric data and create new column extracting experiment start year from the experimental period
+intercrop$Yield_total_intercropping <- as.numeric(gsub(",", ".", intercrop$Yield_total_intercropping))
+intercrop$LER_tot <- as.numeric(gsub(",", ".", intercrop$LER_tot))
+
+intercrop <- intercrop|>
+  mutate(start_year = as.numeric(str_extract(intercrop$Experiment_period, "^[0-9]{4}")))|>
+  mutate(end_year = as.numeric(str_extract(intercrop$Experiment_period, "[0-9]{4}$")))
+
 
 ### create the user interface
 
 ui<-fluidPage(
   theme = bs_theme(bootswatch = "sandstone"),
-  titlePanel("I am adding a title"),
+  titlePanel("Widget 1: Intercropping Yield by Continent"),
   sidebarLayout(
-    sidebarPanel('put my widgets here',
+    sidebarPanel('Areas Researched',
                  radioButtons(
-                   inputId = 'penguin_species',
-                   label='Select relevant metric',
-                   choices=c('Calories','Protein','USD Value','LER Ratio')
-                 ),
-                 selectInput(inputId = 'pt_color',
-                             label='select point color',
-                             choices = c('Roses are red'='red',
-                                         'Violets are purple'='purple',
-                                         'oranges are ...'='orange'))),
-    mainPanel('put my graph here',
-              plotOutput(outputId = 'penguin_plot'),
+                   inputId = 'Continent_type',
+                   label='Select Continent',
+                   choices=c("South Asia","Middle East & North Africa", "Sub-Saharan Africa",
+                             "Latin America & Caribbean","East Asia & Pacific","Europe & Central Asia",
+                             "North America","NA")
+                 )),
+  mainPanel('Intercropping experimental yield over time',
+              plotOutput(outputId = 'intercrop_plot'),
               h3('summary table'),
-              tableOutput(outputId = 'penguin_table'))
+              tableOutput(outputId = 'intercrop_table'))
   )
 )
+
 
 ### create the server function (where all the magic happens from data analysis)
 
 server<-function(input,output){
-  penguin_select<-reactive({
+  intercrop_select<-reactive({
     intercrop_df<-intercrop |>
-      filter(species==input$penguin_species)
+      filter(Continent==input$Continent_type)
   })
   
-  output$penguin_plot<-renderPlot({
-    ggplot(data=penguin_select())+
-      geom_point(aes(x=flipper_length_mm,y=body_mass_g),
-                 color=input$pt_color)
+  output$intercrop_plot<-renderPlot({
+    ggplot(data=intercrop_select(),
+           aes(x=start_year,y=Yield_total_intercropping,fill=Continent))+
+      geom_line(outlier.shape = NA)+
+      geom_point(outlier.shape = NA)+
+      theme(axis.title.x = element_blank(),
+            axis.title.y = element_blank(),
+            legend.position = "none")
   })
   
-  penguin_sum_table<-reactive({
-    penguin_summary_df<-penguins |>
-      filter(species==input$penguin_species)|>
-      group_by(sex)|>
-      summarize(mean_flip=mean(flipper_length_mm,na.rm=TRUE),
-                mean_mass=mean(body_mass_g,na.rm=TRUE))
+  intercrop_sum_table<-reactive({
+    intercrop_summary_df<-intercrop |>
+      filter(Continent==input$Continent_type)|>
+      group_by(start_year)|>
+      summarize(mean_intercropped_yield=mean(Yield_total_intercropping,na.rm=TRUE),
+                mean_LER=mean(LER_tot,na.rm=TRUE))
   })
   
-  output$penguin_table<-renderTable({
-    penguin_sum_table()
+  output$intercrop_table<-renderTable({
+    intercrop_sum_table()
   })
   
 }
